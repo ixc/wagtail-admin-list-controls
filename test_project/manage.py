bff03d6a90d10dfbda5e7a2c191c103684439b03
Mark Finger <markfinger@gmail.com>
2020-04-16 14:38:07 +1000

    Rigged up a django project for testing changes to the app

--- test_project/manage.py
+++ test_project/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangosite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- testproject/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import include, url
-from wagtail.admin import urls as wagtailadmin_urls
-
-urlpatterns = [
-    url(r'^admin/', include(wagtailadmin_urls)),
-]
