b73b5e371213a502d3ab545800185ca28bac0f10
Mark Finger <markfinger@gmail.com>
2020-05-11 17:48:39 +1000

    Fixed a bug where a TogglePanel action without a matching panel would crash the render

--- admin_list_controls/src/admin_list_controls/components/button.js
+++ admin_list_controls/src/admin_list_controls/components/button.js
@@ -19,15 +19,17 @@ export function Button({control}) {
     control.action.forEach(action => {
         if (action.show_panel_toggle_icon) {
             const target_control = state.controls_by_ref[action.ref];
-            const collapsed_by_default = target_control.collapsed;
-            const ref_has_been_toggled = action.ref in state.collapsed_panels_by_ref;
-            if (
-                state.collapsed_panels_by_ref[action.ref] ||
-                (!ref_has_been_toggled && collapsed_by_default)
-            ) {
-                right_icon = <i className="alc__icon alc__icon--panel-toggle icon icon-arrow-down" />;
-            } else {
-                right_icon = <i className="alc__icon alc__icon--panel-toggle icon icon-arrow-up" />;
+            if (target_control) {
+                const collapsed_by_default = target_control.collapsed;
+                const ref_has_been_toggled = action.ref in state.collapsed_panels_by_ref;
+                if (
+                    state.collapsed_panels_by_ref[action.ref] ||
+                    (!ref_has_been_toggled && collapsed_by_default)
+                ) {
+                    right_icon = <i className="alc__icon alc__icon--panel-toggle icon icon-arrow-down" />;
+                } else {
+                    right_icon = <i className="alc__icon alc__icon--panel-toggle icon icon-arrow-up" />;
+                }
             }
         }
     });
